799a3944775d37e6bfc5e93439c4ebe5580493eb
Pinacolada8 <39754336+Pinacolada8@users.noreply.github.com>
2018-11-06 20:31:00 -0200

    REFRESH COMPILATION INSTRUCTIONS

--- Compile.bat
+++ Compile.bat
@@ -4,7 +4,9 @@ color 0a
 
 ECHO Compilando...
 
-SET Arquivos=GameSRC/*
+SET Arquivos=Game-SRC/*.c Game-SRC/SOIL/*.c Game-SRC/GLM/*.c
 
-gcc -o Start %Arquivos% -lfreeglut -lglu32 -lglew32 -lSOIL -lopengl32 -lm -Wall -g
+gcc -o Start %Arquivos% -lfreeglut -lglu32 -lglew32 -lSOIL -lopengl32 -lm -lfmod
+
+ECHO FIM(COMPILADO)
 pause
